fddf817344f519e25e6a42ea0ebb733977b31e8a
Anshul7283 <anshulguptak451@gmail.com>
2024-01-29 23:50:24 +0530

    UPDATE

---
+++ src/main/java/MavenProject1/App.java
@@ -0,0 +1,12 @@
+package MavenProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.print("Hello This is my Practical MST-1");
+    }
+}
